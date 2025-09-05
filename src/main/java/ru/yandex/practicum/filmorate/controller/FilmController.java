1369276f40f18f4e079ff81f1aa8bee831811383
Alex <radist.al@yandex.ru>
2025-09-05 19:28:50 +0300

    ФЗ10

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -16,7 +16,7 @@ import org.slf4j.Logger;
 @RestController
 @RequestMapping("/films")
 public class FilmController {
-    private final static Logger log = LoggerFactory.getLogger(FilmController.class);
+    private static final Logger log = LoggerFactory.getLogger(FilmController.class);
     private final Map<Long, Film> films = new HashMap<>();
 
     //вывод всех фильмов
